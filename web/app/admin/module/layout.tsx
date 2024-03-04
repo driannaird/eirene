ae64ad8a881962ae737d88184db506c714e8d013
Pradana <nextarituenak01@gmail.com>
2024-03-04 14:40:07 +0000

    chore: adding module example pages

---
+++ web/app/admin/module/layout.tsx
@@ -0,0 +1,3 @@
+export default function ModuleLayout({children,}: {children: React.ReactNode}) {
+    return <section className="pt-32 pb-32">{children}</section>
+}
---
+++ web/app/admin/module/page.tsx
@@ -0,0 +1,7 @@
+export default function Module() {
+    return(
+        <div className="max-w-md mx-auto">
+            <h1 className="font-bold italic text-darkColor font-jet">Module Page</h1>
+        </div>
+    )
+}
